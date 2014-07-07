5aaff0b66851ea882a11d6c359b5bce2701248ee
Charles Feinn <chuckfinca@gmail.com>
2014-07-07 15:03:41 -0700

    Initial commit. Adds new UIPickerView delegate & datasource class

---
+++ Meditator/MinutesPickerViewDelegateAndDataSource.h
@@ -0,0 +1,13 @@
+//
+//  TimerPickerViewDelegateAndDataSource.h
+//  Meditator
+//
+//  Created by Charles Feinn on 7/7/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimerPickerViewDelegateAndDataSource : NSObject
+
+@end
---
+++ Meditator/MinutesPickerViewDelegateAndDataSource.m
@@ -0,0 +1,13 @@
+//
+//  TimerPickerViewDelegateAndDataSource.m
+//  Meditator
+//
+//  Created by Charles Feinn on 7/7/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "TimerPickerViewDelegateAndDataSource.h"
+
+@implementation TimerPickerViewDelegateAndDataSource
+
+@end
