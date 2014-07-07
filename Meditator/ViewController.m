606b2ab9d80af2bc809342d25e5db727a88783a1
Charles Feinn <chuckfinca@gmail.com>
2014-07-07 14:26:54 -0700

    Initial Commit

---
+++ Meditator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Meditator
+//
+//  Created by Charles Feinn on 7/7/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MeditatorTests/MeditatorTests.m
@@ -0,0 +1,34 @@
+//
+//  MeditatorTests.m
+//  MeditatorTests
+//
+//  Created by Charles Feinn on 7/7/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MeditatorTests : XCTestCase
+
+@end
+
+@implementation MeditatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
