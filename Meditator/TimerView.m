55be784ec2de1c38a1a9834e5f8847e21f63a903
Charles Feinn <chuckfinca@gmail.com>
2014-07-09 16:08:00 -0700

    simplified the way that the layer is rotated and changed its position

--- Meditator/TimerView.m
+++ Meditator/TimerView.m
@@ -9,7 +9,6 @@
 #import "TimerView.h"
 @interface TimerView ()
 
-@property (nonatomic) CAShapeLayer *progressLayer;
 @property (nonatomic) CGFloat strokeEnd;
 
 @end
@@ -34,36 +33,23 @@
 {
     CGPathRef path = [UIBezierPath bezierPathWithOvalInRect:self.bounds].CGPath;
     
-    CAShapeLayer *backgroundLayer = (CAShapeLayer *)self.layer;
-    [backgroundLayer setPath:path];
-    [backgroundLayer setStrokeColor:[UIColor lightGrayColor].CGColor];
-    [backgroundLayer setLineWidth:1];
-    [backgroundLayer setFillColor:nil];
-    [backgroundLayer setLineDashPattern:@[@1,@3,@4,@2]];
+    CAShapeLayer *layer = (CAShapeLayer *)self.layer;
     
+    [layer setPath:path];
+    [layer setStrokeColor:self.tintColor.CGColor];
+    [layer setLineWidth:2];
+    [layer setStrokeEnd:1];
+    [layer setLineCap:kCALineCapRound];
+    [layer setFillColor:nil];
     
-    
-    CAShapeLayer *progressLayer = [CAShapeLayer new];
-    
-    [progressLayer setPath:path];
-    [progressLayer setStrokeColor:self.tintColor.CGColor];
-    [progressLayer setLineWidth:6];
-    [progressLayer setStrokeEnd:1];
-    [progressLayer setLineCap:kCALineCapRound];
-    [progressLayer setFillColor:nil];
-    
-    [self.layer addSublayer:progressLayer];
-    self.progressLayer = progressLayer;
-    
-    self.transform = CGAffineTransformMakeRotation(3*M_PI/2);
-    self.transform = CGAffineTransformTranslate(self.transform, self.bounds.size.width/2, self.bounds.size.height/2);
-    self.backgroundColor = [UIColor redColor];
+    CATransform3D transf = CATransform3DMakeRotation(3*M_PI_2, 0, 0, 1);
+    self.layer.transform = transf;
 }
 
 -(void)setStrokeEnd:(CGFloat)strokeEnd
 {
     _strokeEnd = strokeEnd;
-    CAShapeLayer *layer = (CAShapeLayer *)self.progressLayer;
+    CAShapeLayer *layer = (CAShapeLayer *)self.layer;
     [layer setStrokeEnd:strokeEnd];
 }
 
