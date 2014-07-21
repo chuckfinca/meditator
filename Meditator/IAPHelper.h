9fcd1a47b7ad7b2dac19b2de9c0da0224915b097
Charles Feinn <chuckfinca@gmail.com>
2014-07-21 16:03:50 -0700

    renamed project to MindTimer and added placeholder for IAP code

---
+++ Meditator/IAPHelper.h
@@ -0,0 +1,13 @@
+//
+//  IAPHelper.h
+//  Meditator
+//
+//  Created by Charles Feinn on 7/21/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IAPHelper : NSObject
+
+@end
---
+++ Meditator/IAPHelper.m
@@ -0,0 +1,13 @@
+//
+//  IAPHelper.m
+//  Meditator
+//
+//  Created by Charles Feinn on 7/21/14.
+//  Copyright (c) 2014 AppSimple. All rights reserved.
+//
+
+#import "IAPHelper.h"
+
+@implementation IAPHelper
+
+@end
