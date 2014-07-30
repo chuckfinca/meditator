b4939925d413020ed5c7f7c910564f86d285d104
Charles Feinn <chuckfinca@gmail.com>
2014-07-29 18:53:00 -0700

    added silent mode detection and alert

--- Meditator/SoundEffectPlayer.m
+++ Meditator/SoundEffectPlayer.m
@@ -12,6 +12,7 @@
 @interface SoundEffectPlayer ()
 
 @property (nonatomic, strong) NSURL *soundUrl;
+@property (nonatomic, strong) NSDate *startTime;
 
 @end
 
@@ -26,12 +27,26 @@
     return self;
 }
 
+void SoundMuteCheckCompletion(SystemSoundID  ssID,void *clientData)
+{
+    SoundEffectPlayer *sep = (__bridge SoundEffectPlayer *)clientData;
+    NSTimeInterval elapsedTime = [[NSDate date] timeIntervalSinceDate:sep.startTime];
+    if(elapsedTime < 0.1){
+        NSLog(@"mute is on");
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Silent Mode Detected" message:@"Unsilence your device to play chimes." delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
+        [alert show];
+    }
+}
+
 -(void)playSoundOrVibrate
 {
+    
     if(self.soundUrl){
         SystemSoundID soundID;
         AudioServicesCreateSystemSoundID((__bridge_retained CFURLRef)self.soundUrl,&soundID);
+        AudioServicesAddSystemSoundCompletion(soundID, CFRunLoopGetMain(), kCFRunLoopDefaultMode, SoundMuteCheckCompletion, (__bridge_retained void *)self);
         AudioServicesPlaySystemSound(soundID);
+        self.startTime = [NSDate date];
     } else {
         AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
     }
