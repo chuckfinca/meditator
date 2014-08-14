5801abe2bc76dc4c94a082d58f7f58864ad344ad
Charles Feinn <chuckfinca@gmail.com>
2014-08-13 17:15:23 -0700

    text tweak

--- Meditator/InstructionsViewController.m
+++ Meditator/InstructionsViewController.m
@@ -41,7 +41,7 @@
     NSString *boldText = @"Introduction to meditation:";
     NSRange boldTextRange = NSMakeRange([heading length], [boldText length]);
     
-    NSString *fullText = [NSString stringWithFormat:@"%@%@\n\n1) Get comfortable! Eliminate unwanted noise, light and distractions from your surroundings.\n\n2) Setup your timer with a duration, chime and other options of your choice. Be sure to choose a duration over which you are likely to be undisturbed.\n\n3) Start meditating. If you don't know how, simply focus on your breath. Pay attention to your senses and be attentive to how your body moves and reacts while you breath in and out.\n\nThoughts will arise and that's ok. Let them come, acknowledge them. Then when you're ready try and return to your breath.\n\nFor best results meditate daily.",heading,boldText];
+    NSString *fullText = [NSString stringWithFormat:@"%@%@\n\n1) Get comfortable! Eliminate unwanted noise, light and distractions from your surroundings.\n\n2) Setup your timer with a duration, chime and other options of your choice. Be sure to choose a duration over which you are likely to be undisturbed.\n\n3) Start meditating. If you don't know how, simply focus on your breath. Pay attention to your senses and be attentive to how your body moves and reacts while you breath in and out.\n\nThoughts will arise and that's ok. Let them come, acknowledge them. Then when you're ready try and return to your breath.\n\n\nFor best results meditate daily.",heading,boldText];
     
     self.instructionsTextView.attributedText = [[NSAttributedString alloc] initWithString:fullText attributes:attributes];
     [self.instructionsTextView.textStorage addAttribute:NSFontAttributeName value:[FontThemer sharedInstance].headline range:boldTextRange];
